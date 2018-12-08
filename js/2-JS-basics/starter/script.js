3ba7e0acac23d6d1e395d29c664fae256eb76eab
David Chorvinsky <firekenace@gmail.com>
2018-12-07 19:40:30 -0500

    ets's do this :q

---
+++ js/2-JS-basics/starter/script.js
@@ -0,0 +1,57 @@
+/*************************
+*Variables and data types
+ */
+
+// var firstName = 'John';
+//
+// console.log(firstName);
+//
+//
+// var lastName = 'Smith';
+// var age = 28;
+//
+// var fullAge = true;
+//
+// console.log(fullAge);
+//
+// var job;
+//
+// job = 'Teacher';
+// console.log(job);
+//
+// // variable naming rules
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+// var if = 23;
+
+
+/*******************
+ variable mutation and type coercion
+ */
+
+var firstName = 'John';
+var age = 28;
+
+//type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = 'false';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+//variable mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
+
+
+/******************
+ *  operatorgit
+ */
